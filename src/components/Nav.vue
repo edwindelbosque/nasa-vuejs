8fa744bc92d8083b3e58c833c2fb134cd28d6d41
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-11 20:23:10 -0700

    Set cursor CSS properties to certain text elements

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,48 +1,49 @@
 <template>
-  <nav>
-    <div>
-      <img alt="NASA logo" src="../assets/logo.png" class="logo" />
-      <h3>Photo of the day</h3>
-    </div>
-    <Date />
-  </nav>
+	<nav>
+		<div>
+			<img alt="NASA logo" src="../assets/logo.png" class="logo" />
+			<h3>Photo of the day</h3>
+		</div>
+		<Date />
+	</nav>
 </template>
 
 <script>
-import Date from "./Date";
+import Date from './Date';
 
 export default {
-  name: "Nav",
-  components: {
-    Date
-  }
+	name: 'Nav',
+	components: {
+		Date
+	}
 };
 </script>
 
 <style scoped>
 nav {
-  margin-top: 20px;
-  display: flex;
-  width: 1000px;
-  justify-content: space-between;
+	margin-top: 20px;
+	display: flex;
+	width: 1000px;
+	justify-content: space-between;
 }
 
 div {
-  display: flex;
-  align-items: center;
+	display: flex;
+	align-items: center;
 }
 
 .logo {
-  width: 100px;
-  height: auto;
+	width: 100px;
+	height: auto;
 }
 
 h3 {
-  color: white;
-  margin-left: 10px;
-  font-size: 20px;
-  background: -webkit-linear-gradient(top right, #69a0ff, #468aff);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+	color: white;
+	margin-left: 10px;
+	font-size: 20px;
+	cursor: default;
+	background: -webkit-linear-gradient(top right, #69a0ff, #468aff);
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
 }
 </style>
