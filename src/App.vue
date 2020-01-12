95bdb7c612c6a91b2136fc1cb521d7fa1b996e8b
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-11 19:25:53 -0700

    Add component structure and style them

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main msg="Hello World" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from "./components/Main";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Main
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: black;
+}
+
+body {
+  background: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+* {
+  background-color: rgba(255, 0, 0, 0.2);
+}
+* * {
+  background-color: rgba(0, 255, 0, 0.2);
+}
+* * * {
+  background-color: rgba(0, 0, 255, 0.2);
+}
+* * * * {
+  background-color: rgba(255, 0, 255, 0.2);
+}
+* * * * * {
+  background-color: rgba(0, 255, 255, 0.2);
+}
+* * * * * * {
+  background-color: rgba(255, 255, 0, 0.2);
+}
+* * * * * * * {
+  background-color: rgba(255, 0, 0, 0.2);
+}
+* * * * * * * * {
+  background-color: rgba(0, 255, 0, 0.2);
+}
+* * * * * * * * * {
+  background-color: rgba(0, 0, 255, 0.2);
 }
 </style>
---
+++ src/components/PhotoHolder.vue
@@ -0,0 +1,52 @@
+<template>
+  <section>
+    <h2>NEBULA GALAXY</h2>
+    <div>
+      <img
+        alt="Photo of the day by NASA."
+        src="https://apod.nasa.gov/apod/image/1912/M33-HaLRGB-RayLiao.jpg"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "PhotoHolder"
+};
+</script>
+
+<style scoped>
+section {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+
+h2 {
+  color: white;
+  background: #043b92;
+  padding: 0px 6px;
+  font-weight: 800;
+  letter-spacing: 1px;
+}
+
+div {
+  margin-top: 10px;
+  width: 1000px;
+  height: fit-content;
+  display: flex;
+  justify-content: center;
+  align-content: flex-start;
+  padding-bottom: 2px;
+  background: rgb(221, 221, 221);
+}
+
+img {
+  width: 996px;
+  height: auto;
+  margin-top: 2px;
+}
+</style>
