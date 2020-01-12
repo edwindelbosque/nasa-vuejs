7c75a8e7ee2e31a83ae9d31a8609725f093c3282
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-11 19:25:53 -0700

    Add component structure and style them

---
+++ src/components/Main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Main">
+    <Nav />
+    <h1>{{ msg }}</h1>
+    <PhotoHolder />
+  </div>
+</template>
+
+<script>
+import Nav from "./Nav";
+import PhotoHolder from "./PhotoHolder";
+
+export default {
+  name: "Main",
+  components: {
+    Nav,
+    PhotoHolder
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.Main {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  width: 1000px;
+  color: black;
+  padding-bottom: 100px;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <div>
+      <img alt="NASA logo" src="../assets/logo.png" class="logo" />
+      <h2>Photo of the day</h2>
+    </div>
+    <Date />
+  </nav>
+</template>
+
+<script>
+import Date from "./Date";
+
+export default {
+  name: "Nav",
+  components: {
+    Date
+  }
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  width: 1000px;
+  justify-content: space-between;
+}
+
+div {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  width: 120px;
+  height: auto;
+}
+
+h2 {
+  color: white;
+}
+</style>
