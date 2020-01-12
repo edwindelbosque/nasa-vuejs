3b2016c5a99571c66d6f4c1a2cfd33fa1e44b245
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-11 20:23:10 -0700

    Set cursor CSS properties to certain text elements

--- src/components/PhotoHolder.vue
+++ src/components/PhotoHolder.vue
@@ -1,54 +1,55 @@
 <template>
-  <section>
-    <h2>NEBULA GALAXY</h2>
-    <div>
-      <img
-        alt="Photo of the day by NASA."
-        src="https://apod.nasa.gov/apod/image/1912/M33-HaLRGB-RayLiao.jpg"
-      />
-    </div>
-  </section>
+	<section>
+		<h2>NEBULA GALAXY</h2>
+		<div>
+			<img
+				alt="Photo of the day by NASA."
+				src="https://apod.nasa.gov/apod/image/1912/M33-HaLRGB-RayLiao.jpg"
+			/>
+		</div>
+	</section>
 </template>
 
 <script>
 export default {
-  name: "PhotoHolder"
+	name: 'PhotoHolder'
 };
 </script>
 
 <style scoped>
 section {
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
+	margin-top: 20px;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: flex-start;
 }
 
 h2 {
-  color: #d5dadd;
-  background: #1a212c;
-  padding: 0px 10px;
-  padding-top: 3px;
-  font-weight: 800;
-  letter-spacing: 1px;
-  text-align: left;
+	color: #d5dadd;
+	background: #1a212c;
+	padding: 0px 10px;
+	padding-top: 3px;
+	font-weight: 800;
+	letter-spacing: 1px;
+	text-align: left;
+	cursor: default;
 }
 
 div {
-  margin-top: 12px;
-  width: 1000px;
-  height: fit-content;
-  display: flex;
-  justify-content: center;
-  align-content: flex-start;
-  padding-bottom: 1px;
-  background: #334055;
+	margin-top: 12px;
+	width: 1000px;
+	height: fit-content;
+	display: flex;
+	justify-content: center;
+	align-content: flex-start;
+	padding-bottom: 1px;
+	background: #334055;
 }
 
 img {
-  width: 998px;
-  height: auto;
-  margin-top: 1px;
+	width: 998px;
+	height: auto;
+	margin-top: 1px;
 }
 </style>
