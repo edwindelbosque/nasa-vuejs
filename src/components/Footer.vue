76d9ded9fac7904a11ab0e42ceaaedb1ceba6c7c
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-01-13 18:30:24 -0700

    Add Footer and add styling to it

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+	<footer>
+		<h4>Built by Edwin Del Bosque</h4>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: 'Footer'
+};
+</script>
+
+<style scoped>
+footer {
+	position: relative;
+	right: 0;
+	left: 0;
+	bottom: 0;
+	height: 80px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-top: 1px solid #4f90ff;
+	margin-top: 100px;
+}
+
+h4 {
+	color: #4f90ff;
+	font-size: 13px;
+	font-weight: 400;
+}
+</style>
